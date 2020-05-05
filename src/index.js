5c4bdf309b28bd5d4b483db442e12512b8903213
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-05-05 20:36:43 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+import "./components/QuestionBox";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
+ 
+class QuizZicle extends Component{
+  state = {
+    questionBank: [],
+    score: 0,
+    responses: 0
+  };
+  getQuestions = () => {
+    quizService().then(question => {
+      this.setState({
+        questionBank: question
+      });
+    });
+  };
+  computeAnswer = (answer, correct) => {
+    if(answer === correct){
+      this.setState({
+        score: this.state.score + 1
+      });
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+    })
+  };
+  playAgain = () => {
+    this.getQuestions();
+    this.setState({
+      score: 0,
+      responses: 0
+    });
+  };
+  componentDidMount(){
+    this.getQuestions();
+  }
+  render(){
+    return(
+      
+      <div className="container">
+        <div className="title">QuizZicle</div>
+        {this.state.questionBank.length > 0 &&
+          this.state.responses < 5 &&
+         this.state.questionBank.map(({question, answers, correct, questionId}) => 
+          (
+            <QuestionBox question={question} options={answers} key={questionId} selected={answer => this.computeAnswer(answer, correct)}/>
+          )
+        )}
+        {this.state.responses === 5 ? <Result score={this.state.score} playAgain={this.playAgain} /> : null}
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<QuizZicle />, document.getElementById("root"));

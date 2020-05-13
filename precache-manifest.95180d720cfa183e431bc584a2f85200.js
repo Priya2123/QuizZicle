ecee56ea62c3418c3e49f74bc54c72f34a056c06
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-05-13 20:10:01 +0530

    Updates

---
+++ precache-manifest.95180d720cfa183e431bc584a2f85200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b25c39c12e1279e34f16cfbd058052d",
+    "url": "/QuizZicle/index.html"
+  },
+  {
+    "revision": "63f169c29fc68666a054",
+    "url": "/QuizZicle/static/css/main.df86dd63.chunk.css"
+  },
+  {
+    "revision": "af2c09a72f67b7efb697",
+    "url": "/QuizZicle/static/js/2.91b5779a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizZicle/static/js/2.91b5779a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f169c29fc68666a054",
+    "url": "/QuizZicle/static/js/main.d00f627d.chunk.js"
+  },
+  {
+    "revision": "5676b3282524617c5700",
+    "url": "/QuizZicle/static/js/runtime-main.e5a2cb2a.js"
+  }
+]);
